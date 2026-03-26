5ecb647b3b3e1a7110dad4d38ec2f230ce1f4cc8
zzigy1 <people8595@naver.com>
2026-03-26 14:15:11 +0900

    에러 수정

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,22 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-const locales = ['ko', 'en', 'zh']
-
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
-  
+  const locales = ['ko', 'en', 'zh']
+
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
   )
-  if (pathnameHasLocale) return
 
-  request.nextUrl.pathname = `/ko${pathname}`
-  return NextResponse.redirect(request.nextUrl)
+  if (pathnameHasLocale) return NextResponse.next()
+
+  // Vercel 서버에 맞게 아주 안전한 방식으로 주소 리다이렉트
+  return NextResponse.redirect(new URL(`/ko${pathname}`, request.url))
 }
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 }
